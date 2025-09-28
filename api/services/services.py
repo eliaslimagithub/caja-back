cd9d98cf44be220aa231abf7845f26658d33def0
eliaslimagithub <mondragonlima@hotmail.com>
2025-09-27 19:42:15 -0600

    Se suben apis de departamentos y lineas

--- api/services/services.py
+++ api/services/services.py
@@ -12,7 +12,7 @@ log.info("Inicio del programa")
 
 class Usuarios:
     def login(self, alias:str, clave:str):
-        log.info("Logueando a: ", alias)
+        log.info("Logueando a: " + alias)
         try:
             sql = f"""
                    select * from usuarios where alias = %s and clave  = %s
@@ -38,24 +38,29 @@ class Usuarios:
 class Productos:
 
     def get_productos(self, id_tienda:int = None, clave:str = None):
-
+        print(f"Tienda: {id_tienda}")
+        print(f"clave: {clave}")
         params =[]
         try:
             sql = f"""
-select * from productos p left join inventarios i
-on i.id_producto = p.id_producto
-where i.id_tienda = %s
+                    select * from productos p 
+                    left join inventarios i
+                    on i.id_producto = p.id_producto
+                    left join tiendas t 
+                    on t.id_tienda = i.id_tienda 
+                    and i.id_tienda = %s
                         """
             params.append(id_tienda)
             if clave:
-                sql += f""" and p.clave like %s;"""
+                sql += f""" where p.clave like %s"""
                 params.append("%" + clave + "%")
 
             conn = get_connection()
             cursor = conn.cursor()
             cursor.execute(sql, params)
-
+            print(f"->> sql {sql}")
             prods = cursor.fetchall()
+            print(f"->>>> prods:  {prods}")
 
         except Exception as e:
             log.warning("Hubo un error al consultar productos", e)
@@ -79,19 +84,19 @@ where i.id_tienda = %s
             conn.close()
 
     def crear_producto(self, id_producto:int, clave:str, descripcion:str, clave_alterna:str,
-                             unidad_entrada:str, editar_precio:bool):
+                             unidad_entrada:str, editar_precio:bool, id_linea:int):
         try:
             conn = get_connection()
             with conn.cursor() as cursor:
-                sql = f"""insert into productos values(%s, %s, %s, %s, %s, %s)
+                sql = f"""insert into productos values(%s, %s, %s, %s, %s, %s, %s)
                           ON DUPLICATE KEY UPDATE clave = values(clave), 
                                                   descripcion = values(descripcion),
                                                   clave_alterna = coalesce(values(clave_alterna), clave_alterna),
                                                   unidad_entrada = coalesce(values(unidad_entrada), unidad_entrada),
-                                                  editar_precio = coalesce(values(editar_precio), editar_precio);
+                                                  editar_precio = coalesce(values(editar_precio), editar_precio, id_linea);
                        """
                 cursor.execute(sql, (id_producto, clave, descripcion, clave_alterna,
-                               unidad_entrada, editar_precio))
+                               unidad_entrada, editar_precio, id_linea))
 
                 conn.commit()
                 log.info("->>> Producto procesado correctamente")
@@ -101,15 +106,16 @@ where i.id_tienda = %s
         finally:
             conn.close()
 
-    def upsert_precios(self, id_tienda:int, id_poducto:int, precios:list):
+    def upsert_precios(self, id_poducto:int, precios:list):
         try:
+            print("->>>> 1")
             conn = get_connection()
             with conn.cursor() as cursor:
                 for p in precios:
-                    sql = f"""insert into precios values(%s, %s, %s, %s)
+                    sql = f"""insert into precios values(%s, %s, %s)
                                               ON DUPLICATE KEY UPDATE precio = coalesce(values(precio), precio);
                                            """
-                    cursor.execute(sql, (id_tienda, id_poducto, p.get("id_precio"), p.get("precio")))
+                    cursor.execute(sql, (id_poducto, p.get("id_precio"), p.get("precio")))
 
             conn.commit()
             log.info("->>> Precios registrados correctamente")
@@ -296,6 +302,113 @@ where id_politica = %s
             raise e
         finally:
             conn.close()
+
+class Departamentos:
+    def get_departamentos(self, clave):
+        log.info(f"Departamento: {clave}")
+        params =[]
+        try:
+            conn = get_connection()
+            cursor = conn.cursor()
+            sql = f"""
+                    select * from departamentos
+                        """
+            if clave:
+                sql += f""" where clave like %s"""
+                params.append("%" + clave + "%")
+                cursor.execute(sql, params)
+            else:
+                cursor.execute(sql)
+
+            dptos = cursor.fetchall()
+
+        except Exception as e:
+            log.warning("Hubo un error al consultar departamentos", e)
+            raise e
+        finally:
+            cursor.close()
+            conn.close()
+        return dptos
+
+    def crear_departamento(self, clave:str, descripcion:str):
+        try:
+            conn = get_connection()
+            with conn.cursor() as cursor:
+                sql = f"""insert into departamentos values(null, %s, %s);
+                       """
+                cursor.execute(sql, (clave, descripcion))
+
+                conn.commit()
+                log.info(f"->>> departamento {descripcion} creado correctamente")
+            return cursor.rowcount
+        except Exception as e:
+            raise e
+        finally:
+            conn.close()
+
+class Lineas:
+    def get_lineas(self, clave):
+        log.info(f"Consultando linea: {clave}")
+        params =[]
+        try:
+            conn = get_connection()
+            cursor = conn.cursor()
+            sql = f"""
+                    select * from lineas
+                        """
+            if clave:
+                sql += f""" where clave like %s"""
+                params.append("%" + clave + "%")
+                cursor.execute(sql, params)
+            else:
+                cursor.execute(sql)
+
+            dptos = cursor.fetchall()
+
+        except Exception as e:
+            log.warning("Hubo un error al consultar lineas", e)
+            raise e
+        finally:
+            cursor.close()
+            conn.close()
+        return dptos
+
+    def crear_linea(self, clave:str, descripcion:str, id_departamento:int):
+
+        try:
+            conn = get_connection()
+            with conn.cursor() as cursor:
+                sql = f"""insert into lineas (clave, descripcion_linea, id_departamento) values(%s, %s, %s);
+                       """
+                cursor.execute(sql, (clave, descripcion, id_departamento))
+
+                conn.commit()
+                log.info(f"->>> linea {descripcion} creada correctamente")
+            return cursor.rowcount
+        except Exception as e:
+            raise e
+        finally:
+            conn.close()
+
+    def update_linea(self, id_departamento:int, clave:str, descripcion:str, id_linea:int):
+
+        try:
+            conn = get_connection()
+            with conn.cursor() as cursor:
+                sql = f"""
+                        update lineas set clave = %s, descripcion_linea = %s, id_departamento = %s
+                        where id_linea = %s
+                              """
+                cursor.execute(sql, (clave, descripcion, id_departamento, id_linea))
+                conn.commit()
+            return cursor.rowcount
+        except Exception as e:
+            log.error("Hubo un error modificar la linea: ", e)
+            raise e
+        finally:
+            conn.close()
+
+
 
 
 #ret = Productos()
